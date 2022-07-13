9857a82e5fca55c962a6f72350166bf0e4551686
Mudasar Rauf <mudassar.rauf@gmail.com>
2022-07-13 22:49:58 +0100

    applied k8s deployment for docker based pgsql

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -7,10 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Add services to the container.
 // TODO: for now using in memory database, but should be replaced with a real database
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseInMemoryDatabase("PlatformService"));
+// builder.Services.AddDbContext<AppDbContext>(options =>
+//     options.UseInMemoryDatabase("PlatformService"));
 // TODO: convert the implementation to pooled factory
-//builder.Services.AddDbContext<AppDbContext>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("default")));
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("default")));
 //builder.Services.AddPooledDbContextFactory<AppDbContext>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("default")));
 // TODO: toggle for sql server
 // builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("default")));
@@ -24,13 +24,13 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Initalize the database with data
-await app.InitializeAsync();
+// await app.InitializeAsync();
 
 // Run the migrations
-// using var scope = app.Services.CreateScope();
-// var contextFactory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<AppDbContext>>();
-// await using var dbContext = contextFactory.CreateDbContext();
-// await dbContext.Database.MigrateAsync();
+using var scope = app.Services.CreateScope();
+//var contextFactory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<AppDbContext>>();
+await using var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+await dbContext.Database.MigrateAsync();
 
 
 
